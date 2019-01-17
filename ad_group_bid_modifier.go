28b48a99f7d60f5e926954f82b22ace213b928d9
fbernard <fabien.bernard@twenga.com>
2019-01-17 16:21:58 +0100

    add adgroup bid modifier

--- ad_group_bid_modifier.go
+++ ad_group_bid_modifier.go
@@ -1,8 +1,8 @@
 package gads
 
 import (
-//	"encoding/xml"
-//	"fmt"
+	"encoding/xml"
+	//	"fmt"
 )
 
 type AdGroupBidModifierService struct {
@@ -13,12 +13,57 @@ func NewAdGroupBidModifierService(auth *Auth) *AdGroupBidModifierService {
 	return &AdGroupBidModifierService{Auth: *auth}
 }
 
-/*
+type AdGroupBidModifierOperations map[string][]AdGroupBidModifier
+
 type AdGroupBidModifier struct {
-  CampaignId        int64     `xml:"campaignId"`
-  AdGroupId         int64     `xml:"adGroupId"`
-  Criterion         Criterion `xml:"criterion"`
-  BidModifier       float64   `xml:"bidModifier"`
-  BidModifierSource string    `xml:"bidModifierSource"`
+	CampaignId        int64     `xml:"campaignId"`
+	AdGroupId         int64     `xml:"adGroupId"`
+	Criterion         Criterion `xml:"criterion"`
+	BidModifier       float64   `xml:"bidModifier"`
+	BidModifierSource string    `xml:"bidModifierSource"`
 }
-*/
+
+// Mutate takes a budgetOperations and creates, modifies or destroys the associated budgets.
+func (s *AdGroupBidModifierService) Mutate(bidmOperations AdGroupBidModifierOperations) (resp []AdGroupBidModifier, err error) {
+	type bidmOperation struct {
+		Action             string             `xml:"operator"`
+		AdGroupBidModifier AdGroupBidModifier `xml:"operand"`
+	}
+	operations := []bidmOperation{}
+	for action, bidms := range bidmOperations {
+		for _, bidm := range bidms {
+			operations = append(operations, bidmOperation{Action: action, AdGroupBidModifier: bidm})
+		}
+	}
+	respBody, err := s.Auth.request(
+		adGroupBidModifierServiceUrl,
+		"mutate",
+		struct {
+			XMLName xml.Name
+			Ops     []bidmOperation `xml:"operations"`
+		}{
+			XMLName: xml.Name{
+				Space: baseUrl,
+				Local: "mutate",
+			},
+			Ops: operations,
+		},
+	)
+	if err != nil {
+		return resp, err
+	}
+	mutateResp := struct {
+		BaseResponse
+		AdGroupBidModifiers []AdGroupBidModifier `xml:"rval>value"`
+	}{}
+	err = xml.Unmarshal([]byte(respBody), &mutateResp)
+	if err != nil {
+		return resp, err
+	}
+
+	if len(mutateResp.PartialFailureErrors) > 0 {
+		err = mutateResp.PartialFailureErrors
+	}
+
+	return mutateResp.AdGroupBidModifiers, err
+}
